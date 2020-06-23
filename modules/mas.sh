35c59f9b7d4c7316822d8d9dc82cad8ce623bcdd
Damian Tykalowski <d47zm3@gmail.com>
2020-06-23 10:07:18 +0200

    mas fixup

--- modules/mas.sh
+++ modules/mas.sh
@@ -9,13 +9,12 @@ readonly module_log_file="${script_dir}/log/${module_name}.log"
 
 mas_apps=(
   "1475387142"
-  )
+)
 
 decho "initialising ${module_name} module..."
 
 mkdir -p "${script_dir}/log"
 true > "${module_log_file}"
-fi
 
 decho "installing mas utils..."
 if ! mas install "${mas_apps[@]}" >> "${module_log_file}" 2>&1
